fb5f10b8c73b0502ce4ae75bb98f1a3b954841e6
RanaHisham18 <hishamrana00@gmail.com>
2023-11-12 23:21:05 +0200

    shared pref add and read

--- app/src/main/java/com/example/runtimevaluechangeapp/MainActivity.kt
+++ app/src/main/java/com/example/runtimevaluechangeapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.runtimevaluechangeapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.runtimevaluechangeapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        val sharedPref = getSharedPreferences("ranaPref", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+       // val data = binding.editText.text.toString()
+        binding.button.setOnClickListener {
+            val data = binding.editText.text.toString()
+
+            editor.putString("version", data)
+            editor.apply()
+
+            binding.textView.text = sharedPref.getString("version", "This is the default value")
+        }
+
+
+
     }
 }
